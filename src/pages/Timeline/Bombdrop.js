9683d4ff5f8cc50f1e67972674ca6a1f12ade975
prajpurohit680 <prajpurohit680@gmail.com>
2023-08-21 18:35:41 +0530

    bg changed for timeline

--- src/pages/Timeline/Bombdrop.js
+++ src/pages/Timeline/Bombdrop.js
@@ -2,10 +2,10 @@ import React, { useState, useEffect, useRef } from 'react';
 import './bombdrop.css';
 import Bomb from '../../assets/timeline/bomb.png'
 import Blast from '../../assets/timeline/blast.gif'
-import { unmountComponentAtNode } from 'react-dom';
 
 const BombDroppingEffect = () => {
-    const end = 2700;
+    const end = 3200;
+    const start = 1000;
     const [scrollY, setScrollY] = useState(0);
     const [scrollDirection, setScrollDirection] = useState('down');
     const render = useRef(false);
@@ -16,7 +16,7 @@ const BombDroppingEffect = () => {
 
 
             const newScrollY = window.scrollY;
-            if (newScrollY > end + 50) {
+            if (newScrollY > end + 30) {
                 render.current = false;
             }
             setScrollDirection(newScrollY > scrollY ? 'down' : 'up');
@@ -39,7 +39,7 @@ const BombDroppingEffect = () => {
                 !render ? <div> </div> :
 
                     scrollY > end ? <img src={Blast} className={`bomb-image ${(scrollY > end && scrollDirection === 'down' && scrollY < end + 200) ? 'show' : ''}`} alt='blast' /> :
-                        <img src={Bomb} className={`bomb-image ${(scrollY > 1000 && scrollDirection === 'down' && scrollY < end) ? 'show' : ''}`} alt='bomb' />
+                        <img src={Bomb} className={`bomb-image ${(scrollY > start && scrollDirection === 'down' && scrollY < end) ? 'show' : ''}`} alt='bomb' />
             }
 
 
